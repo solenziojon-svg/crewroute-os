11d186020847d64b035f50403a5a657dbd98a404
solenziojon-svg <solenziojon@gmail.com>
2026-05-27 02:42:47 -0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-// supabase.js
-const SUPABASE_URL = "https://cmzjukukfbqutyunrrl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtemp1a2V1a2ZicXR5dW5ycmwiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcxNzI5NjAwMCwiZXhwIjoyMDMyODcyMDAwfQ.your-real-key-here"; 
-
-export const supabaseConfig = {
-  url: SUPABASE_URL,
-  anonKey: SUPABASE_ANON_KEY
-};postgresql://postgres:[YOUR-PASSWORD]@db.cmzjukeukfbqutyunrrl.supabase.co:5432/postgres
