8dee1783488fa0e7d1c043ba611debd1ca870ce4
solenziojon-svg <solenziojon@gmail.com>
2026-05-27 20:15:10 -0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', fontFamily: 'system-ui' }}>
+      <h1>CrewRoute OS</h1>
+      <p>Build test - this should work.</p>
+    </main>
+  );
+}
