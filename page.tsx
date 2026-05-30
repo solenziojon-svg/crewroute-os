025ac939c697d6758373bc9118f56a23fc7c0ed6
solenziojon-svg <solenziojon@gmail.com>
2026-05-29 20:41:20 -0700

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, fontFamily: 'system-ui' }}>
-      <h1>CrewRoute OS</h1>
-      <p>Deployment test - working!</p>
-    </main>
-  );
-}
